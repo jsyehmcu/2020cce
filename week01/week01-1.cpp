a1c34da59ec87d405b3fe280a67dcb69dba4b0af
jsyehmcu <79677445+jsyehmcu@users.noreply.github.com>
2021-03-12 11:50:38 +0800

    Update and rename a.cpp to week01/week01-1.cpp

---
+++ week01/week01-1.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+  printf("Hello World\n");  //Q: 改檔名 A: 在左上角, 直接改檔名, 另外直接按下 week01/week01-1.cpp 就會分出目錄
+} 
+//A:下面直接改內容
+今天上課教的 GitHub 使用方法, 把你實習課的 8 題, 放上 GitHub
+
+作法: New 新的 repository 倉庫, 取個名字 2020cce, 記得裡面要有 README.md
+
+把它的 main 改名成 gh-pages
+
+接下來在 README.
